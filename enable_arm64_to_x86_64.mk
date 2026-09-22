288b4e529fdb9af7bf58cc4c1ad9e94eb6eaa876
icy-person <kar.fars.1388@gmail.com>
2026-09-22 11:36:16 -0400

    fix: restore ARM64 product license and shipped IR-check configuration

--- enable_arm64_to_x86_64.mk
+++ enable_arm64_to_x86_64.mk
@@ -1,13 +1,40 @@
-# ARM64 guest -> x86_64 host Berberis configuration.
+#
+# Copyright (C) 2026 utzcoz
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 include frameworks/libs/binary_translation/berberis_config.mk
 
 PRODUCT_PACKAGES += $(BERBERIS_PRODUCT_PACKAGES_ARM64_TO_X86_64)
-PRODUCT_SYSTEM_PROPERTIES += ro.dalvik.vm.native.bridge=libberberis_arm64.so
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.dalvik.vm.native.bridge=libberberis_arm64.so
+
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.dalvik.vm.isa.arm64=x86_64 \
     ro.enable.native.bridge.exec=1
+
+# Skip the translator's per-translation MachineIR validation passes on the
+# shipped image. Host tests keep validation enabled by default.
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.berberis.flags=disable-ir-check
+
 PRODUCT_SOONG_NAMESPACES += frameworks/libs/native_bridge_support/android_api/libc
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += $(BERBERIS_DISTRIBUTION_ARTIFACTS_ARM64)
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    $(BERBERIS_DISTRIBUTION_ARTIFACTS_ARM64)
+
 BUILD_BERBERIS := true
 BUILD_BERBERIS_ARM64_TO_X86_64 := true
 $(call soong_config_set,berberis,translation_arch,arm64_to_x86_64)
